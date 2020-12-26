1936a169639f0a5fe92789f1a47a0e3d25f2fc25
TOMO-YOSHI <lucky.paddy10@gmail.com>
2020-12-26 15:25:33 -0800

    Finish guess a number app

--- guess-a-number-app/components/Header.js
+++ guess-a-number-app/components/Header.js
@@ -1,28 +1,49 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Platform } from "react-native";
+import TitleText from "./TitleText";
 import colors from "../constants/colors";
 
 const Header = (props) => {
   return (
-    <View style={styles.header}>
-      <Text style={styles.headerTitle}>{props.title}</Text>
+    <View
+      style={{
+        ...styles.headerBase,
+        ...Platform.select({
+          ios: styles.headerIOS,
+          android: styles.headerAndroid,
+        }),
+      }}
+    >
+      <TitleText style={styles.headerTitle}>{props.title}</TitleText>
     </View>
   );
 };
 export default Header;
 
 const styles = StyleSheet.create({
-  header: {
+  headerBase: {
     width: "100%",
     height: 90,
     paddingTop: 36,
-    backgroundColor: colors.primary,
     alignItems: "center",
     justifyContent: "center",
   },
+  headerIOS: {
+    backgroundColor: "white",
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 1,
+  },
+  headerAndroid: {
+    backgroundColor: colors.primary,
+    // borderBottomColor: "transparent",
+    // borderBottomWidth: 0,
+  },
+  // headerTitle: {
+  //   color: "black",
+  //   fontSize: 18,
+  //   fontFamily: "open-sans-bold",
+  // },
   headerTitle: {
-    color: "black",
-    fontSize: 18,
-    fontFamily: "open-sans-bold",
+    color: Platform.OS === "ios" ? colors.primary : "white",
   },
 });
--- guess-a-number-app/components/MainButton.js
+++ guess-a-number-app/components/MainButton.js
@@ -1,18 +1,39 @@
 import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TouchableNativeFeedback,
+  Platform,
+} from "react-native";
 
 import colors from "../constants/colors";
 
-const MainButton = (props) => (
-  <TouchableOpacity activeOpacity={0.6} onPress={props.onPress}>
-    <View style={styles.button}>
-      <Text style={styles.buttonText}>{props.children}</Text>
+const MainButton = (props) => {
+  let ButtonComponent = TouchableOpacity;
+
+  if (Platform.OS === "android" && Platform.Version >= 21) {
+    ButtonComponent = TouchableNativeFeedback;
+  }
+
+  return (
+    <View style={styles.buttonContainer}>
+      <ButtonComponent activeOpacity={0.6} onPress={props.onPress}>
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>{props.children}</Text>
+        </View>
+      </ButtonComponent>
     </View>
-  </TouchableOpacity>
-);
+  );
+};
 export default MainButton;
 
 const styles = StyleSheet.create({
+  buttonContainer: {
+    borderRadius: 25,
+    overflow: "hidden",
+  },
   button: {
     backgroundColor: colors.primary,
     paddingVertical: 12,
